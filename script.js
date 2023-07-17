c4a1c68d28c589737fe03742a65dcfa96da56955
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-07-17 12:25:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function fibonacci(num) {
 // your code here
+	function fibonacci(num) {
+  if (num === 0) {
+    return 0;
+  } else if (num === 1 || num === 2) {
+    return 1;
+  } else {
+    return fibonacci(num - 1) + fibonacci(num - 2);
+  }
+}
+
+	
 }
 
 module.exports = fibonacci;
